c6263d282319691fce9952ed22fe5ff16453ced4
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-08 13:49:30 +0530

    Add files via upload

---
+++ Print_short_Variable123.java
@@ -0,0 +1,24 @@
+//first  print of short variable 
+
+class Print_short_Variable123
+
+{
+
+//This is main method
+public static void main(String [] psv)
+{
+//declaration and intialization of short  variable 
+
+short variable1=10;
+
+//printing short variable 
+
+System.out.println(variable1);
+
+}
+
+}
+/*output:
+1:Compile time:Success
+2:RunTime:Success: 10
+*/
