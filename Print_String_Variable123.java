722d8223dccd819395f0734eac8857b743267347
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-08 14:22:10 +0530

    Print_String_Variable123

---
+++ Print_String_Variable123.java
@@ -0,0 +1,24 @@
+//first  print of String variable 
+
+class Print_String_Variable123
+
+{
+
+//This is main method
+public static void main(String [] psv)
+{
+//declaration and intialization of char variable 
+
+ String variable1="pranjal";
+
+//printing string variable 
+
+System.out.println(variable1);
+
+}
+
+}
+/*output:
+1:Compile time:Success
+2:RunTime:Success: pranjal
+*/
