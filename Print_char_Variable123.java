9eecefac6a306bda05e6f792c87b1f3b89295157
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-08 14:08:32 +0530

    Print_char_Variable123

---
+++ Print_char_Variable123.java
@@ -0,0 +1,24 @@
+//first  print of charvariable 
+
+class Print_char_Variable123
+
+{
+
+//This is main method
+public static void main(String [] pcv)
+{
+//declaration and intialization of char variable 
+
+char variable1=40;
+
+//printing char variable 
+
+System.out.println(variable1);
+
+}
+
+}
+/*output:
+1:Compile time:Success
+2:RunTime:Success: C
+*/
