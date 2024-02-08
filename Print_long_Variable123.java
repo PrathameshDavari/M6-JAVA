1a2db7634d89381b620ab1de90ae3e1395955e37
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-08 14:02:20 +0530

    Add files via upload

---
+++ Print_long_Variable123.java
@@ -0,0 +1,24 @@
+//first  print of long variable 
+
+class Print_long_Variable123
+
+{
+
+//This is main method
+public static void main(String [] plv)
+{
+//declaration and intialization of  long variable 
+
+long variable1=30;
+
+//printing  long variable 
+
+System.out.println(variable1);
+
+}
+
+}
+/*output:
+1:Compile time:Success
+2:RunTime:Success: 30
+*/
