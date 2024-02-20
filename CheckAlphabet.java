76c482da178d05bdc72c05aa6553039a2fad2bc3
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-20 18:20:26 +0530

    CheckAlphabet

---
+++ CheckAlphabet.java
@@ -0,0 +1,14 @@
+class 	CheckAlphabet
+{
+public static void main(String [] args)
+{
+char value ='A';
+String result =(value>65&&value<=90)||(value>=47&&value<=122)?("the given char is Alphabet"):("the given char is not Alphabet");
+System.out.println(result);
+
+
+}
+}
+
+
+
