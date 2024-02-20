f89b5f2287014a1098754b90385079b6c1f38136
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-20 18:21:53 +0530

    ConditionClass

---
+++ Codition.java
@@ -0,0 +1,27 @@
+class Codition
+{
+public static void main(String[]args)
+{
+int age1=25;
+int age2=80;
+int price=56;
+int price1=80;
+int distance =15;
+int distance1 =15;
+
+
+boolean result1 = age1<age2;
+boolean result2 = price<price1;
+boolean result3 = distance<distance1;
+System.out.println("L.H.S is < R.H.S"+result1);
+System.out.println("L.H.S is < R.H.S"+result2);
+System.out.println("L.H.S is < R.H.S"+result3);
+
+
+
+
+
+}
+
+
+}
