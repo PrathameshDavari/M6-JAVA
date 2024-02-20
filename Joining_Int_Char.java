9175470fe94850c51048ed1d118790b57db69fb3
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-20 19:05:42 +0530

    Add files via upload

---
+++ Joining_Int_Char.java
@@ -0,0 +1,17 @@
+//This is example of joining number and varible and character variable
+
+class Joining_Int_Char
+   {
+public static void main(String[]args)
+
+{
+int a=4;
+String b="sid";
+String c="davari";
+System.out.println(b+ " "+ a +" " + c);
+
+
+}
+
+
+}
