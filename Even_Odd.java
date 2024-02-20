8ad9c789361e7f21068006eedafed368fb386b1d
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-20 18:49:01 +0530

    Even_Odd

---
+++ Even_Odd.java
@@ -0,0 +1,10 @@
+class Even_Odd
+{
+public static void main(String[]args)
+{
+char c ='a';
+String result =(c%2==0)?("the given function is Even"):("the given function is Odd");
+System.out.println(result);
+}
+
+}
