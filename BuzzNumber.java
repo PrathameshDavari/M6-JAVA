15548ed1351071cd95b15fbe58daca8c04aa1976
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-02-20 18:18:51 +0530

    BuzzNumber

---
+++ BuzzNumber.java
@@ -0,0 +1,13 @@
+class BuzzNumber
+{
+public static void main(String [] args)
+{
+
+int a =98;
+String result= (a%7==0||a%10==7)?("the given no is buzz"):("the given no is not buzz");
+System.out.println(result);
+
+}
+}
+
+
