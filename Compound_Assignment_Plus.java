9e74d21970242127e9d45ac279781cf00c394cf8
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-03-05 00:25:46 +0530

    Add files via upload

---
+++ Compound_Assignment_Plus.java
@@ -0,0 +1,15 @@
+class Compound_Assignment_Plus
+{
+	public static void main(String[] cao)
+	{
+		int age=25;
+		age+=5;
+		System.out.println(age);
+	}
+}
+
+/*
+Output:
+CT: success;
+RT: success: 30
+*/
