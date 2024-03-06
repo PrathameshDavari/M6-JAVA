41f9b71251dd00b4f966461594b385df29d42bbb
PrathameshDavari <159007941+PrathameshDavari@users.noreply.github.com>
2024-03-06 12:12:26 +0530

    Add files via upload

---
+++ Do_While_Lopp.java
@@ -0,0 +1,13 @@
+class  Do_While_Lopp_EX_3
+{
+public static void main(String[]ars)
+{
+int v=3;
+do
+{
+System.out.println(v);
+v=v+3;
+}
+while(v<=30&&v%==2);
+}
+}
---
+++ Do_While_Lopp_EX_3.java
@@ -0,0 +1,18 @@
+class  Do_While_Lopp_EX_3
+{
+public static void main(String[]ars)
+{
+int v=1;
+do
+{
+int a=v*3;
+if(a%2==0)
+{
+System.out.println(a);
+
+}
+v++;
+}while(v<=10);
+
+}
+}
